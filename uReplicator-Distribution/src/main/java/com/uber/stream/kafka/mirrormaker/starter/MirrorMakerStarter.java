b661ea03678beee906e173ec5672c55fa2cbaca5
ksambhav <kumar.sambhav.jain@gmail.com>
2017-03-16 15:48:26 +0530

    docker in progress.

--- uReplicator-Distribution/src/main/java/com/uber/stream/kafka/mirrormaker/starter/MirrorMakerStarter.java
+++ uReplicator-Distribution/src/main/java/com/uber/stream/kafka/mirrormaker/starter/MirrorMakerStarter.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015-2016 Uber Technology Inc. (streaming-core@uber.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,12 @@
  */
 package com.uber.stream.kafka.mirrormaker.starter;
 
-import java.util.Arrays;
-
+import com.uber.stream.kafka.mirrormaker.controller.ControllerStarter;
+import kafka.mirrormaker.MirrorMakerWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.uber.stream.kafka.mirrormaker.controller.ControllerStarter;
-
-import kafka.mirrormaker.MirrorMakerWorker;
+import java.util.Arrays;
 
 /**
  * This is the entry point to start mirror maker controller and worker.
@@ -33,24 +31,24 @@ import kafka.mirrormaker.MirrorMakerWorker;
  */
 public class MirrorMakerStarter {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(MirrorMakerStarter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MirrorMakerStarter.class);
 
-  public static void main(String[] args) throws Exception {
-    if (args.length > 1) {
-      if (args[0].equalsIgnoreCase("startMirrorMakerController")) {
-        LOGGER.info("Trying to start MirrorMaker Controller with args: {}", Arrays.toString(args));
-        ControllerStarter.main(args);
-      } else if (args[0].equalsIgnoreCase("startMirrorMakerWorker")) {
-        LOGGER.info("Trying to start MirrorMaker Worker with args: {}", Arrays.toString(args));
-        MirrorMakerWorker.main(args);
-      } else {
-        LOGGER.error("Start script should provide the module(startMirrorMakerController/startMirrorMakerWorker)"
-            + " to start as the first parameter! Current args: {}", Arrays.toString(args));
-      }
-    } else {
-      LOGGER.error(
-          "Start script doesn't provide enough parameters! Current args: {}.", Arrays.toString(args));
+    public static void main(String[] args) throws Exception {
+        if (args.length > 1) {
+            if (args[0].equalsIgnoreCase("startMirrorMakerController")) {
+                LOGGER.info("Trying to start MirrorMaker Controller with args: {}", Arrays.toString(args));
+                ControllerStarter.main(args);
+            } else if (args[0].equalsIgnoreCase("startMirrorMakerWorker")) {
+                LOGGER.info("Trying to start MirrorMaker Worker with args: {}", Arrays.toString(args));
+                MirrorMakerWorker.main(args);
+            } else {
+                LOGGER.error("Start script should provide the module(startMirrorMakerController/startMirrorMakerWorker)"
+                        + " to start as the first parameter! Current args: {}", Arrays.toString(args));
+            }
+        } else {
+            LOGGER.error(
+                    "Start script doesn't provide enough parameters! Current args: {}.", Arrays.toString(args));
+        }
     }
-  }
 
 }
